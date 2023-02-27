05966e2284ac9df77372bda08fe371cdabbc3991
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-27 16:41:24 +0100

    first try

---
+++ app.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        names: [],
+      }
+    },
+
+    methods: {
+        generateEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+
+            this.names.push(res.data.response)
+            
+            })
+        },
+
+        mounted(){
+            for (let i = 0; i < 10; i++){
+                this.generateEmail()
+            }
+        }
+    },
+    
+}).mount('#app')
